d9c1792975f69e2dc311ed2775515b4e1d3f3134
Yoann MALLEMANCHE <yoann.mallemanche@gmail.com>
2021-08-28 18:33:43 +0200

    fix: script multi DB not working

--- scripts/pg-init-scripts/create-multiple-postgresql-databases.sh
+++ scripts/pg-init-scripts/create-multiple-postgresql-databases.sh
@@ -9,7 +9,7 @@ function create_user_and_database() {
   psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
 	    CREATE USER $database;
 	    CREATE DATABASE $database;
-	    GRANT ALL PRIVILEGES ON DATABASE $database TO $database;
+	    GRANT ALL PRIVILEGES ON DATABASE $database TO $POSTGRES_USER;
 EOSQL
 }
 
