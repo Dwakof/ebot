3f01417c9eaa9d9a65c14ac486dca0fd1089eaaa
Ihsan Isik <git@ihsan.dev>
2021-09-15 14:09:30 +0200

    Cleanup

--- src/config/index.js
+++ src/config/index.js
@@ -144,7 +144,7 @@ const store = new Confidence.Store({
             openWeatherApiKey : { $env : 'OPEN_WEATHER_API_KEY' },
             LocationIQApiKey  : { $env : 'LOCATION_IQ_API_KEY' }
         },
-        googleCse    : {
+        googleImages    : {
             apiKey   : { $env : 'GOOGLE_CSE_API_KEY' },
             engineId : { $env : 'GOOGLE_CSE_ENGINE_ID' }
         }
--- src/modules/tools/services/googleImages.js
+++ src/modules/tools/services/googleImages.js
@@ -35,7 +35,6 @@ module.exports = class GoogleImagesService extends Service {
     init() {
         this.#api = Got.extend({
             prefixUrl: 'https://www.googleapis.com',
-            // responseType : 'json'
         });
     }
     /**
@@ -47,8 +46,8 @@ module.exports = class GoogleImagesService extends Service {
 
         const { body, statusCode } = await this.#api.get('customsearch/v1', {
             searchParams : { 
-                key: this.client.settings.plugins.googleCse.apiKey,
-                cx: this.client.settings.plugins.googleCse.engineId,
+                key: this.client.settings.plugins.googleImages.apiKey,
+                cx: this.client.settings.plugins.googleImages.engineId,
                 q: query,
                 searchType: 'image',
                 filter: 1,
