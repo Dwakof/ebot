9faf501f246473b688c9f13b019edf7678baf2b2
Yoann MALLEMANCHE <yoann.mallemanche@gmail.com>
2021-11-12 01:58:52 +0100

    style: fix ESLint

--- src/core/util.js
+++ src/core/util.js
@@ -3,6 +3,7 @@
 const Fs   = require('fs/promises');
 const Path = require('path');
 
+// eslint-disable-next-line no-unused-vars
 const { GuildMember, User, Guild, Channel, Message, Role, Snowflake } = require('discord.js');
 const { EventEmitter }                                                = require('events');
 
